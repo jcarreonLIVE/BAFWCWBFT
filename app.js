51ab80f55e3ef7789f6fb614a7889fc76f4ae6a2
jcarreonLIVE <74849398+jcarreonLIVE@users.noreply.github.com>
2021-05-25 17:42:42 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,6 @@
       var confirmPassword = "1234";
       var password = document.getElementById("pswd").value;
       if (password == confirmPassword) {
-        window.location = "welkom.html";
+        window.location = "/BAFWCWBFT/welkom.html";
       }
     }
