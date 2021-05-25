77f69ff3e15652a4c09a8b67b5eb3a5688aaf49f
jcarreonLIVE <74849398+jcarreonLIVE@users.noreply.github.com>
2021-05-25 17:58:28 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
     function checkPswd() {
-      var confirmPassword = "YEEZY";
+      var confirmPassword = "1234";
       var password = document.getElementById("pswd").value;
       if (password == confirmPassword) {
         window.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
