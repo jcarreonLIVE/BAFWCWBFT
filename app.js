fb2bb9785b3e775a61d21560cec007a98d705c53
jcarreonLIVE <74849398+jcarreonLIVE@users.noreply.github.com>
2021-05-25 17:40:56 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
     function checkPswd() {
-      var confirmPassword = "VATT-SFFIRIF-1845";
+      var confirmPassword = "1234";
       var password = document.getElementById("pswd").value;
       if (password == confirmPassword) {
         window.location = "welkom.html";
