6468f286ea5bf65c395813ec5538a95db7ab66bf
jcarreonLIVE <74849398+jcarreonLIVE@users.noreply.github.com>
2021-05-25 19:19:34 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,6 @@
       var confirmPassword = "1234";
       var password = document.getElementById("pswd").value;
       if (password == confirmPassword) {
-        window.location = "https://www.youtube.com/watch?v=iik25wqIuFo";
+        window.location = "https://vatt-aaaaaaa-1111.netlify.app/";
       }
     }
